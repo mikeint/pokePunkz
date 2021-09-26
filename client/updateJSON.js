80dfe40c166fea15cbd4dfa943c6cacb46bb0e02
michael <michael.sansone@hotmail.com>
2021-09-26 10:39:24 -0400

    saving stuff

--- client/updateJSON.js
+++ client/updateJSON.js
@@ -3,6 +3,6 @@ const fs = require("fs")
 let file = fs.readFileSync("./src/layout/files/masterJSON.json")
 
 JSON.parse(file).forEach((data, i) => {
-        fs.writeFileSync("./metadata/"+(i+1), JSON.stringify(data))
+        fs.writeFileSync("./data/"+(i+1), JSON.stringify(data))
     }
 )
